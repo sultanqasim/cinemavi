99dc24f80925b7d08f0aeff679517a2d4373939f
Sultan Qasim Khan <sultanqasim@yandex.com>
2022-02-04 23:10:32 -0500

    noise_reduction: use 5x5 kernel
    3x faster for slight reduction in NR quality

--- noise_reduction.c
+++ noise_reduction.c
@@ -7,16 +7,18 @@
 #include "convolve.h"
 #include "ycrcg.h"
 
-// convolves image using 7x7 gaussian kernel
+#define KERNEL_SIZE 5
+#define KERNEL_VARIANCE 1.3
+
+// convolves image using 5x5 gaussian kernel
 // outputs weighted average of original image and convolved image, weighted based on luminance
 // luminance is (R+G+B) / sqrt(3)
 // no NR applied to pixels with luminance values above intensity argument
-#define KERNEL_SIZE 7
 void noise_reduction_rgb(const float *img_in, float *img_out, unsigned int width, unsigned int height,
         float intensity)
 {
     float kernel[KERNEL_SIZE * KERNEL_SIZE];
-    gaussian_kernel(kernel, KERNEL_SIZE, 1.41421);
+    gaussian_kernel(kernel, KERNEL_SIZE, KERNEL_VARIANCE);
 
     float *img_smooth = (float *)malloc(width * height * 3 * sizeof(float));
     if (img_smooth == NULL) {
@@ -75,7 +77,7 @@ void noise_reduction_ycrcg(const float *img_in, float *img_out, unsigned int wid
         float intensity_lum, float intensity_chrom)
 {
     float kernel[KERNEL_SIZE * KERNEL_SIZE];
-    gaussian_kernel(kernel, KERNEL_SIZE, 1.41421);
+    gaussian_kernel(kernel, KERNEL_SIZE, KERNEL_VARIANCE);
 
     float *img_smooth = (float *)malloc(width * height * 3 * sizeof(float));
     if (img_smooth == NULL) {
