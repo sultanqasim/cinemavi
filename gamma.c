9736847f94d72a66e73964f134a20711c99d15ce
Sultan Qasim Khan <sultanqasim@yandex.com>
2022-02-05 11:34:18 -0500

    implement filmic base curve option

--- gamma.c
+++ gamma.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <assert.h>
 #include "gamma.h"
 
 // generate lut for gamma encoding a linear space image
@@ -9,8 +10,77 @@ void gamma_gen_lut(uint8_t *lut, uint8_t bit_depth)
     double G = 8.0 / bit_depth;
     double i_scale = 1.0 / (1 << bit_depth);
 
+    assert(bit_depth >= 8);
+    assert(bit_depth <= 16);
+
     for (int i = 0; i < 1 << bit_depth; i++)
-        lut[i] = 256 * pow(i * i_scale, G);
+        lut[i] = 255.999 * pow(i * i_scale, G);
+}
+
+// apply cubic base curve before gamma encoding
+// suggested coefficients: a=-1.7, b=2.6, c=0.1
+void gamma_gen_lut_cubic(uint8_t *lut, uint8_t bit_depth,
+        double a, double b, double c)
+{
+    double G = 8.0 / bit_depth;
+    double i_scale = 1.0 / (1 << bit_depth);
+
+    assert(bit_depth >= 8);
+    assert(bit_depth <= 16);
+    assert(a + b + c < 1.0000001);
+
+    for (int i = 0; i < 1 << bit_depth; i++) {
+        // base curve function: y = f(x) = a*x^3 + b*x^2 + c*x
+        // f(x) must be in [0, 1] for x in [0, 1]
+        double x = i * i_scale;
+        double y = a*x*x*x + b*x*x + c*x;
+        lut[i] = 255.999 * pow(y, G);
+    }
+}
+
+// apply a filmic base curve before gamma encoding
+// shadow is the slope at the black end
+// gamma controls how midtones are boosted and highlights compressed
+// suggested coefficients: gamma=0.3, shadow=0.8
+void gamma_gen_lut_filmic(uint8_t *lut, uint8_t bit_depth,
+        double gamma, double shadow)
+{
+    double G = 8.0 / bit_depth;
+    double i_scale = 1.0 / (1 << bit_depth);
+
+    /* base curve function: y = a*f(x) + shadow*g(x)
+     * y must be in [0, 1] for x in [0, 1]
+     *
+     * f(x) = x^(gamma/x)
+     *
+     * let k = 0.05
+     * a = 1 + shadow/ln(k)
+     * a is defined to ensure that y=1 when x=1
+     *
+     * g(x) = b * (1 - (k^x))
+     * b = -1 / ((1 - k) * ln(k))
+     * b is defined to ensure d/dx g(x) = 1 when x=0
+     * with this b definition, g(1) = -1/ln(k)
+     * g(x) goes from 0 to -1/ln(k), as x goes from 0 to 1
+     * slope of g(x) at x=0 is 1
+     */
+    const double k = 0.05;
+    double ln_k = log(k);
+    double a = 1 + shadow/ln_k;
+    double b = -1 / ((1-k) * ln_k);
+    double b_shadow = b * shadow;
+
+    assert(bit_depth >= 8);
+    assert(bit_depth <= 16);
+    assert(gamma > 0);
+    assert(shadow >= 0);
+    assert(shadow < -ln_k);
+
+    for (int i = 0; i < 1 << bit_depth; i++) {
+        double x = i * i_scale;
+        double y = a*pow(x, gamma/x) + b_shadow*(1 - pow(k, x));
+        lut[i] = 255.999 * pow(y, G);
+    }
 }
 
 // assumes length of lut >= highest value in img_in
--- gamma.h
+++ gamma.h
@@ -12,6 +12,16 @@ extern "C" {
 // length of lut should be 1 << bit_depth
 void gamma_gen_lut(uint8_t *lut, uint8_t bit_depth);
 
+// apply cubic base curve before gamma encoding
+void gamma_gen_lut_cubic(uint8_t *lut, uint8_t bit_depth,
+        double a, double b, double c);
+
+// apply a filmic base curve before gamma encoding
+// shadow is the slope at the black end
+// gamma controls how midtones are boosted and highlights compressed
+void gamma_gen_lut_filmic(uint8_t *lut, uint8_t bit_depth,
+        double gamma, double shadow);
+
 // assumes length of lut >= highest value in img_in
 void gamma_encode(const uint16_t *img_in, uint8_t *img_out, uint16_t width, uint16_t height,
         const uint8_t *lut);
