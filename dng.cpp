770037a8c267892d9ba3b2f240ec8dfe71a2bb4a
Sultan Qasim Khan <sultanqasim@yandex.com>
2022-02-05 21:13:34 -0500

    Simplify and correct DNG generation

--- dng.cpp
+++ dng.cpp
@@ -24,8 +24,6 @@ int arv_buffer_to_dng(ArvBuffer *buffer, const char *dng_name, const char *camer
     dng_image.SetImageWidth(width);
     dng_image.SetImageLength(height);
     dng_image.SetRowsPerStrip(height);
-    const unsigned int active_area[4] = {0, 0, height, width};
-    dng_image.SetActiveArea(active_area);
     dng_image.SetSamplesPerPixel(1);
     const uint16_t bpp[1] = {16};
     dng_image.SetBitsPerSample(1, bpp);
@@ -44,16 +42,22 @@ int arv_buffer_to_dng(ArvBuffer *buffer, const char *dng_name, const char *camer
     const uint8_t cpat[4] = {0, 1, 1, 2};
     dng_image.SetCFAPattern(4, cpat);
 
-    // Colour calibration
-    ColourMatrix XYZtoRGB;
-    colour_matrix(&XYZtoRGB, 0, 0.0, -0.45, 1.7);
-    dng_image.SetCalibrationIlluminant1(17); // StdA
+    // Colour calibration (use Adobe RGB primaries)
+    // http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+    ColourMatrix XYZtoRGB_D50 = {.m={
+        1.9624274, -0.6105343, -0.3413404,
+       -0.9787684,  1.9161415,  0.0334540,
+        0.0286869, -0.1406752,  1.3487655
+    }};
+    ColourMatrix XYZtoRGB_D65 = {.m={
+        2.0413690, -0.5649464, -0.3446944,
+       -0.9692660,  1.8760108,  0.0415560,
+        0.0134474, -0.1183897,  1.0154096
+    }};
+    dng_image.SetCalibrationIlluminant1(23); // D50
     dng_image.SetCalibrationIlluminant2(21); // D65
-    dng_image.SetColorMatrix1(3, XYZtoRGB.m);
-    dng_image.SetColorMatrix2(3, XYZtoRGB.m);
-    const double idmat[9] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
-    dng_image.SetCameraCalibration1(3, idmat);
-    dng_image.SetCameraCalibration2(3, idmat);
+    dng_image.SetColorMatrix1(3, XYZtoRGB_D50.m);
+    dng_image.SetColorMatrix2(3, XYZtoRGB_D65.m);
     dng_image.SetAsShotWhiteXY(1.0 / 3, 1.0 / 3);
 
     size_t imsz;
