7d2dd86c1da1ba0117c99b0f3a12c62e2f288707
Sultan Qasim Khan <sultanqasim@yandex.com>
2022-02-06 17:24:23 -0500

    Offline CMRAW processing script
    To generate TIFF from CMR

--- Makefile
+++ Makefile
@@ -6,8 +6,9 @@ GLIB_DIR = /opt/homebrew/Cellar/glib/2.70.2
 
 CFLAGS = -Wall -Wextra -I$(ARAVIS_DIR)/src -I$(ARAVIS_DIR)/build/src
 CFLAGS += -I$(GLIB_DIR)/include/glib-2.0 -I$(GLIB_DIR)/lib/glib-2.0/include
-LFLAGS = -L$(ARAVIS_DIR)/build/src -laravis-0.8
-LFLAGS += -L$(GLIB_DIR)/lib -lgobject-2.0
+
+LFLAGS_ARV = -L$(ARAVIS_DIR)/build/src -laravis-0.8
+LFLAGS_ARV += -L$(GLIB_DIR)/lib -lgobject-2.0
 
 ifeq ($(DEBUG), 1)
 CFLAGS += -fsanitize=address -g -O0
@@ -16,9 +17,14 @@ else
 CFLAGS += -O3 -ffast-math
 endif
 
-all: single_capture
+all: single_capture cmraw_process
 
-single_capture: single_capture.o dng.opp colour_xfrm.o debayer.o convolve.o noise_reduction.o gamma.o pipeline.o cmraw.o
+LIB_OBJS = dng.opp colour_xfrm.o debayer.o convolve.o noise_reduction.o gamma.o pipeline.o cmraw.o
+
+single_capture: single_capture.o $(LIB_OBJS)
+	$(CPP) $(LFLAGS) $(LFLAGS_ARV) $^ -o $@
+
+cmraw_process: cmraw_process.o $(LIB_OBJS)
 	$(CPP) $(LFLAGS) $^ -o $@
 
 %.o: %.c
---
+++ cmraw_process.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "cmraw.h"
+#include "pipeline.h"
+#include "dng.h"
+
+static void cinemavi_generate_tiff(const void *raw, const CMCaptureInfo *cinfo,
+        const char *fname)
+{
+    uint8_t *rgb8 = (uint8_t *)malloc(cinfo->width * cinfo->height * 3);
+    if (rgb8 != NULL) {
+        ImagePipelineParams params = {
+            .exposure = 1.0,
+            .warmth = 0.0,
+            .tint = 0.15,
+            .hue = 0.0,
+            .sat = 1.0,
+            .nr_lum = 150.0,
+            .nr_chrom = 600.0,
+            .gamma = 0.25,
+            .shadow = 1.0
+        };
+        printf("Processing image...\n");
+        pipeline_process_image(raw, rgb8, cinfo, &params);
+        printf("Image processed.\n");
+
+        int tiff_stat = rgb8_to_tiff(rgb8, cinfo->width, cinfo->height, fname);
+        if (tiff_stat != 0) printf("Error %d writing TIFF.\n", tiff_stat);
+        else printf("TIFF written to: %s\n", fname);
+    }
+    free(rgb8);
+}
+
+static bool endswith(const char *s, const char *suffix)
+{
+    size_t s_len = strlen(s);
+    size_t e_len = strlen(suffix);
+
+    if (e_len > s_len) return false;
+
+    return memcmp(s + s_len - e_len, suffix, e_len) ? false : true;
+}
+
+int main (int argc, char **argv)
+{
+    if (argc != 3) {
+        printf("Usage: %s [cmr_name] [tiff_name]\n", argv[0]);
+        return -1;
+    }
+
+    if (!endswith(argv[1], ".cmr")) {
+        printf("Invalid input extension: %s\n", argv[1]);
+        return -1;
+    }
+
+    if (!endswith(argv[2], ".tiff")) {
+        printf("Invalid output extension: %s\n", argv[2]);
+        return -1;
+    }
+
+    CMCaptureInfo cinfo;
+    void *raw = NULL;
+
+    int status = cmraw_load(&raw, &cinfo, argv[1]);
+    if (status != 0) {
+        printf("Error %d loading RAW file.\n", status);
+    } else {
+        cinemavi_generate_tiff(raw, &cinfo, argv[2]);
+    }
+
+    free(raw);
+
+    return status;
+}
