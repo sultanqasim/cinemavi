d87a3ff0c6f59400b75c5e62db3383559d3b186f
Sultan Qasim Khan <sultanqasim@yandex.com>
2022-02-12 17:22:20 -0500

    Implement auto exposure capture loop

--- single_capture.c
+++ single_capture.c
@@ -2,14 +2,26 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include <arv.h>
 
 #include "dng.h"
 #include "cmraw.h"
 #include "pipeline.h"
+#include "auto_exposure.h"
+#include "debayer.h"
 
-static void cinemavi_camera_configure(ArvCamera *camera, double shutter_us, double gain_db, GError **error)
+static void cinemavi_camera_configure_exposure(ArvCamera *camera, double shutter_us,
+        double gain_db, GError **error)
+{
+    if (!(*error)) arv_camera_set_gain(camera, gain_db, error);
+    if (!(*error)) shutter_us *= pow(10, (gain_db - arv_camera_get_gain(camera, error)) / 20);
+    if (!(*error)) arv_camera_set_exposure_time(camera, shutter_us, error);
+}
+
+static void cinemavi_camera_configure(ArvCamera *camera, double shutter_us, double gain_db,
+        GError **error)
 {
     int wmin, wmax, hmin, hmax;
 
@@ -21,10 +33,9 @@ static void cinemavi_camera_configure(ArvCamera *camera, double shutter_us, doub
     if (!(*error)) arv_camera_set_binning(camera, 1, 1, error);
 
     // Exposure
-    if (!(*error)) arv_camera_set_exposure_time_auto(camera, ARV_AUTO_OFF, error);
-    if (!(*error)) arv_camera_set_exposure_time(camera, shutter_us, error);
     if (!(*error)) arv_camera_set_gain_auto(camera, ARV_AUTO_OFF, error);
-    if (!(*error)) arv_camera_set_gain(camera, gain_db, error);
+    if (!(*error)) arv_camera_set_exposure_time_auto(camera, ARV_AUTO_OFF, error);
+    cinemavi_camera_configure_exposure(camera, shutter_us, gain_db, error);
 
     // White balance (make it neutral)
     if (!(*error)) arv_camera_set_string(camera, "BalanceWhiteAuto", "Continuous", error);
@@ -118,6 +129,80 @@ static void cinemavi_generate_cmr(const void *raw, const CMRawHeader *cmrh,
         printf("CMR written to: %s\n", fname);
 }
 
+static int cinemavi_auto_exposure(ArvCamera *camera, const ExposureLimits *limits,
+        double *shutter, double *gain)
+{
+    GError *error = NULL;
+    ExposureParams params = {.shutter_us = 1000, .gain_dB = 5};
+    ExposureParams params2;
+    double change_factor = 0.0;
+    int ret = 0;
+
+    cinemavi_camera_configure(camera, params.shutter_us, params.gain_dB, &error);
+
+    while (change_factor < 0.95 || change_factor > 1.05) {
+        if (error) {
+            ret = -EIO;
+            break;
+        }
+
+        // Acquire a single buffer
+        ArvBuffer *buffer = arv_camera_acquisition(camera, 0, &error);
+
+        if (!ARV_IS_BUFFER(buffer)) {
+            ret = -EIO;
+            break;
+        }
+
+        ArvPixelFormat pfmt = arv_buffer_get_image_pixel_format(buffer);
+        if (pfmt != ARV_PIXEL_FORMAT_BAYER_RG_12P) {
+            ret = -EINVAL;
+            g_clear_object(&buffer);
+            break;
+        }
+
+        unsigned int width = arv_buffer_get_image_width(buffer);
+        unsigned int height = arv_buffer_get_image_height(buffer);
+        uint16_t rgw = width >> 1;
+        uint16_t rgh = height >> 1;
+
+
+        size_t imsz;
+        const void *imbuf = (const void *)arv_buffer_get_data(buffer, &imsz);
+
+        uint16_t *bayer12 = (uint16_t *)malloc(width * height * sizeof(uint16_t));
+        if (bayer12 == NULL) {
+            ret = -ENOMEM;
+            g_clear_object(&buffer);
+            break;
+        }
+        unpack12_16(bayer12, imbuf, width * height, false);
+        g_clear_object(&buffer);
+
+        uint16_t *img_rgb = (uint16_t *)malloc(rgw * rgh * 3 * sizeof(uint16_t));
+        if (img_rgb == NULL) {
+            ret = -ENOMEM;
+            free(bayer12);
+            break;
+        }
+
+        debayer22_binned(bayer12, img_rgb, width, height);
+        free(bayer12);
+
+        change_factor = auto_exposure(img_rgb, rgw, rgh, 1600, 3600);
+        free(img_rgb);
+
+        calculate_exposure(&params, &params2, limits, change_factor);
+        params = params2;
+        cinemavi_camera_configure_exposure(camera, params.shutter_us, params.gain_dB, &error);
+    }
+
+    *shutter = params.shutter_us;
+    *gain = params.gain_dB;
+
+    return ret;
+}
+
 static bool endswith(const char *s, const char *suffix)
 {
     size_t s_len = strlen(s);
@@ -143,9 +228,28 @@ int main (int argc, char **argv)
         const char *camera_model = arv_camera_get_model_name(camera, NULL);
         printf("Found camera '%s %s'\n", camera_make, camera_model);
 
+        // Auto exposure settings
+        ExposureLimits limits;
+        limits.shutter_targ_low = 8000;
+        limits.shutter_targ_high = 30000;
+        limits.gain_targ_low = 5;
+        limits.gain_targ_high = 15;
+        if (!error) arv_camera_get_gain_bounds(camera, &limits.gain_min, &limits.gain_max, &error);
+        if (!error) arv_camera_get_exposure_time_bounds(camera, &limits.shutter_min, &limits.shutter_max, &error);
+        if (error) goto err;
+
+        // Determine exposure
+        double shutter_us;
+        double gain_dB;
+        printf("Running auto exposure...\n");
+        if (cinemavi_auto_exposure(camera, &limits, &shutter_us, &gain_dB)) {
+            printf("Auto exposure failed.\n");
+            goto err;
+        }
+
+        printf("Shutter: %.1f us Gain: %.2f dB\n", shutter_us, gain_dB);
+
         // Set configuration
-        const double shutter_us = 30000;
-        const double gain_dB = 5;
         cinemavi_camera_configure(camera, shutter_us, gain_dB, &error);
         if (error) goto err;
 
